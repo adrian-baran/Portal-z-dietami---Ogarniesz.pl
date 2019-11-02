cbb1ea2546c70c62186dda31ca17d77fb3130c31
adrian-baran <56650940+adrian-baran@users.noreply.github.com>
2019-11-02 19:36:26 +0100

    Update mybody.class.php

--- mybody.class.php
+++ mybody.class.php
@@ -1,6 +1,7 @@
 <?php
 class mybody {
 	
+	
 //obliczanie zapotrzebowania kalorycznego
 public function ileKcal($plec,$waga,$wzrost,$wiek,$tryb){
 	//jeżeli mężczyzna(true)
@@ -12,10 +13,28 @@ public function ileKcal($plec,$waga,$wzrost,$wiek,$tryb){
 		
 	}
 	
-	$zapotrzebowanie = $przemiana * $tryb;
+	$zapotrzebowanie = round($przemiana * $tryb);
 	
 	return $zapotrzebowanie;
 	
+}
+
+//obliczanie wskaźnika BMI
+
+public function getBmi ($waga,$wzrost) {
+	$bmi = round($waga / pow(($wzrost/100),2),2); //wzrost w metrach oraz zaokrąglenie do dwóch miejsc po przecinku
+	return $bmi; 
+}
+
+//sprawdzanie BMI
+public function checkBmi ($bmi) { 
+$result = '';
+if ($bmi < 18.5) $result .= ' Masz niedowagę<br />';
+if (18.5 <= $bmi  && $bmi < 25) $result .= ' Twoja waga jest prawidłowa!<br />';
+if (25 <= $bmi && $bmi < 30) $result .= ' Masz nadwagę<br />';
+if ($bmi >= 30) $result .= ' To już oznacza otyłość, nie poddawaj się!<br />';
+
+return $result;
 }
 
 
